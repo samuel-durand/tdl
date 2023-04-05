572750ca42b7d45c2128b22ca9fa37673e6a4203
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-04-05 11:18:11 +0200

    all work in async

--- todo list.php
+++ todo list.php
@@ -53,93 +53,92 @@ $completed_tasks = $stmt->fetchAll();
 <!--<script src="./add.js"></script>-->
     <h1>Liste des tâches</h1>
 
-    <h2>Tâches en cours</h2>
-	<?php if (count($tasks) > 0) { ?>
-	<table>
-		<thead>
-			<tr>
-				<th>ID</th>
-				<th>Titre</th>
-				<th>Description</th>
-				<th>Action</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ($tasks as $task) { ?>
-				<tr>
-					<td><?php echo $task['id']; ?></td>
-					<td><?php echo $task['title']; ?></td>
-					<td><?php echo $task['description']; ?></td>
-
-					<td>
-						<form method="post" id="update">
-							<input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
-							<input type="text" name="title" value="<?php echo $task['title']; ?>">
-							<textarea name="description"><?php echo $task['description']; ?></textarea>
-							<button type="submit">Modifier</button>
-                            <script src="./update.js"></script>
-						</form>
- 
-
-                        <form method="post" id="complete">
+    <?php 
+if (count($tasks) > 0) { 
+?>
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Titre</th>
+            <th>Description</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php 
+        foreach ($tasks as $task) { 
+            if (!$task['complete']) { // Si la tâche n'est pas terminée, on l'affiche
+        ?>
+            <tr>
+                <td><?php echo $task['id']; ?></td>
+                <td><?php echo $task['title']; ?></td>
+                <td><?php echo $task['description']; ?></td>
+                <td>
+                    <form method="post" id="update">
                         <input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
-                         <button type="submit">Terminer la tâche</button>
-                        </form>
-
-                        <script src="complete.js"></script>
+                        <input type="text" name="title" value="<?php echo $task['title']; ?>">
+                        <textarea name="description"><?php echo $task['description']; ?></textarea>
+                        <button type="submit">Modifier</button>
+                        <script src="./update.js"></script>
                     </form>
 
-                    
+                    <form method="post" id="complete">
+                        <input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
+                        <button type="submit">Terminer la tâche</button>
+                    </form>
+                    <script src="complete.js"></script>
+                </td>
+            </tr>
+        <?php 
+            }
+        } 
+        ?>
+    </tbody>
+</table>
+<?php 
+} else { 
+?>
+<p>Aucune tâche en cours n'a été trouvée.</p>
+<?php 
+} 
+?>
 
-					</td>
-				</tr>
-
-			<?php } ?>
-		</tbody>
-	</table>
-	<?php } else { ?>
-		<p>Aucune tâche en cours n'a été trouvée.</p>
-	<?php } ?>
-
-	<h2>Tâches terminées</h2>
-	<?php if (count($tasks) > 0) { ?>
-	<table>
-		<thead>
-			<tr>
-				<th>ID</th>
-				<th>Titre</th>
-				<th>Description</th>
-                <th>terminée</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ($tasks as $task) { ?>
-                <tr>
-  <td><?php echo $task['id']; ?></td>
-  <td class="completed"><?php echo $task['title']; ?></td>
-  <td class="completed"><?php echo $task['description']; ?></td>
-  <td class="completed"><?php echo $task['complete']; ?></td>
-  <td>
-    <form method="post" id="delete"<?php echo $task['id']; ?>">
-    
-      <input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
-      <button type="submit">Supprimer</button>
-
-    </form>
-    <script src="delete.js"></script>
-
-  </td>
-</tr>
-
-                
-
-			<?php } ?>
-		</tbody>
-	</table>
-	<?php } else { ?>
-		<p>Aucune tâche terminée n'a été trouvée.</p>
-	<?php } ?>
-
+    <h2>Tâches terminées</h2>
+<?php if (count($tasks) > 0) { ?>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titre</th>
+                <th>Description</th>
+                <th>Terminée</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($tasks as $task) {
+                if ($task['complete']) { ?>
+                    <tr>
+                        <td><?php echo $task['id']; ?></td>
+                        <td class="completed"><?php echo $task['title']; ?></td>
+                        <td class="completed"><?php echo $task['description']; ?></td>
+                        <td class="completed"><?php echo $task['complete']; ?></td>
+                        <td>
+                            <form method="post" id="delete"<?php echo $task['id']; ?>">
+                                <input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
+                                <button type="submit">Supprimer</button>
+                            </form>
+                            <script src="delete.js"></script>
+                        </td>
+                    </tr>
+                <?php }
+            } ?>
+        </tbody>
+    </table>
+<?php } else { ?>
+    <p>Aucune tâche terminée n'a été trouvée.</p>
+<?php } ?>
 </body>
 </html>
 
