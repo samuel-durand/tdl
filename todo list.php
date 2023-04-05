01c72d87fff632b39e62c5dd4b45c02c32c4eb08
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-04-05 20:11:22 +0200

    style update

--- todo list.php
+++ todo list.php
@@ -10,6 +10,9 @@ if (!isset($_SESSION['username'])) {
     exit;
 }
 
+date_default_timezone_set("Europe/Paris");
+
+
 if (date("H") < 18)
 $bienvenue = "bonjour et bienvenue " .
 $_SESSION['username'];
@@ -26,8 +29,11 @@ $stmt->execute();
 $tasks = $stmt->fetchAll();
 ?>
 
+<!doctype html>
+
 <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
     <title>Liste des tâches</title>
 
     <!-- Ajoutez le lien CDN pour Tailwind ici -->
@@ -37,10 +43,10 @@ $tasks = $stmt->fetchAll();
 <body>
 
 <?php include('header.php') ?>
-<h3 class="text-light"><?php echo $bienvenue ?></h3>
-<div class="w-96 mx-auto">
-  <h1 class="text-2xl font-bold mb-4">Ajouter une liste</h1>
-  <form method="post" id="add" class="bg-white rounded-lg shadow-lg p-6">
+<h3 id="welcome" class="text-light"><?php echo $bienvenue ?></h3>
+<div>
+  <h1 id="add-list" class=" font-bold mb-4">Ajouter une liste</h1>
+  <form method="post" id="add" id="add-box" class="bg-white rounded-lg shadow-lg p-6">
     <div class="mb-4">
       <label for="title" class="block font-bold mb-2">Titre :</label>
       <input type="text" name="title" id="title" class="w-full border rounded py-2 px-3">
@@ -57,47 +63,48 @@ $tasks = $stmt->fetchAll();
   </form>
 </div>
 
-<h1 class="text-3xl mb-5">Liste des tâches</h1>
-
-<?php 
-if (count($tasks) > 0) { 
-?>
-<ul class="pl-8">
+<div class="flex flex-col justify-center items-center ">
+    <h1 class="text-3xl mb-5">Liste des tâches</h1>
     <?php 
-    foreach ($tasks as $task) { 
-        if (!$task['complete']) { // Si la tâche n'est pas terminée, on l'affiche
+    if (count($tasks) > 0) { 
     ?>
-    <li class="mb-3">
-        <form method="post" id="update">
-            <input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
-            <div class="flex justify-center items-center">
-                <input type="text" name="title" value="<?php echo $task['title']; ?>" class="w-full border rounded px-2 py-1">
-                
-            </div>
-            <textarea name="description" class="w-full border rounded px-2 py-1 mt-2"><?php echo $task['description']; ?></textarea>
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-1 px-4 ml-2">Modifier</button>
+    <ul class="">
+        <?php 
+        foreach ($tasks as $task) { 
+            if (!$task['complete']) { // Si la tâche n'est pas terminée, on l'affiche
+        ?>
+        <li class="bg-white rounded-lg shadow-lg p-6">
+            <form method="post" id="update">
+                <input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
+                <input type="text" name="title" value="<?php echo $task['title']; ?>" class="w-full border rounded px-2 py-1 mb-2">
+                <textarea name="description" class="w-full border rounded px-2 py-1 mb-2"><?php echo $task['description']; ?></textarea>
+                <div class="flex justify-between items-center">
+                    <p class="text-gray-600"><?php echo $task['date']; ?></p>
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-1 px-4">Modifier</button>
+                </div>
+                <script src="./update.js"></script>
+            </form>
 
-            <script src="./update.js"></script>
-        </form>
-
-        <form method="post" id="complete">
-            <input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
-            <div class="flex justify-center">
-                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold rounded py-1 px-4 mt-2">Terminer la tâche</button>
-            </div>
-            <script src="complete.js"></script>
-        </form>
-    </li>
+            <form method="post" id="complete">
+                <input type="hidden" name="task_id" value="<?php echo $task['id']; ?>">
+                <div class="flex justify-center">
+                    <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold rounded py-1 px-4 mt-2">Terminer la tâche</button>
+                </div>
+                <script src="complete.js"></script>
+            </form>
+        </li>
+        <?php 
+            }
+        } 
+        ?>
+    </ul>
     <?php 
-        }
-    } 
+    } else {
+        echo "Aucune tâche trouvée.";
+    }
     ?>
-</ul>
-<?php 
-} else {
-    echo "Aucune tâche trouvée.";
-}
-?>
+</div>
+
 
 <h2 class="text-xl font-bold mb-4">Tâches terminées</h2>
 <?php if (count($tasks) > 0) { ?>
@@ -107,6 +114,7 @@ if (count($tasks) > 0) {
                 <th class="border border-gray-400 px-4 py-2 text-center">ID</th>
                 <th class="border border-gray-400 px-4 py-2 text-center">Titre</th>
                 <th class="border border-gray-400 px-4 py-2 text-center">Description</th>
+                <th class="border border-gray-400 px-4 py-2 text-center">Heure ajouter</th>
                 <th class="border border-gray-400 px-4 py-2 text-center">Heure terminée</th>
                 <th class="border border-gray-400 px-4 py-2 text-center">Terminée</th>
                 <th class="border border-gray-400 px-4 py-2 text-center">Action</th>
@@ -119,6 +127,7 @@ if (count($tasks) > 0) {
                         <td class="border border-gray-400 px-4 py-2 text-center"><?php echo $task['id']; ?></td>
                         <td class="border border-gray-400 px-4 py-2"><?php echo $task['title']; ?></td>
                         <td class="border border-gray-400 px-4 py-2"><?php echo $task['description']; ?></td>
+                        <td class="border border-gray-400 px-4 py-2 text-center"><?php echo $task['date']; ?></td>
                         <td class="border border-gray-400 px-4 py-2 text-center"><?php echo $task['date_end']; ?></td>
                         <td class="border border-gray-400 px-4 py-2 text-center">Oui</td>
                         <td class="border border-gray-400 px-4 py-2 text-center">
